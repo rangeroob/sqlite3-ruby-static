ce030ec54ce45107dcecbba4b95f41cd2864a9bd
Aaron Patterson <aaron.patterson@gmail.com>
2017-01-04 10:08:29 -0800

    this needs a chomp
    (cherry picked from commit c8806a2fc367eb40fe87bc4818925e005be2a606)

--- ext/sqlite3/extconf.rb
+++ ext/sqlite3/extconf.rb
@@ -6,6 +6,29 @@ require 'mkmf'
 
 RbConfig::MAKEFILE_CONFIG['CC'] = ENV['CC'] if ENV['CC']
 
+
+
+ldflags = cppflags = nil
+if RbConfig::CONFIG["host_os"] =~ /darwin/
+  begin
+    brew_prefix = `brew --prefix sqlite3`.chomp
+    ldflags   = "#{brew_prefix}/lib"
+    cppflags  = "#{brew_prefix}/include"
+    pkg_conf  = "#{brew_prefix}/lib/pkgconfig"
+
+    # pkg_config should be less error prone than parsing compiler
+    # commandline options, but we need to set default ldflags and cpp flags
+    # in case the user doesn't have pkg-config installed
+    ENV['PKG_CONFIG_PATH'] ||= pkg_conf
+  rescue
+  end
+end
+
+pkg_config("sqlite3")
+
+# --with-sqlite3-{dir,include,lib}
+dir_config("sqlite3", cppflags, ldflags)
+
 if RbConfig::CONFIG["host_os"] =~ /mswin/
   $CFLAGS << ' -W3'
 end
